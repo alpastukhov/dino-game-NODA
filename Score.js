e1e8dda51b4bc7b6b6dc7fa93e27afcd9b32686a
Alex <113027623+alpastukhov@users.noreply.github.com>
2025-11-15 19:49:06 +0700

    Update Score.js

--- Score.js
+++ Score.js
@@ -26,16 +26,17 @@ export default class Score {
   draw() {
     const highScore = Number(localStorage.getItem(this.HIGH_SCORE_KEY));
     const y = 20 * this.scaleRatio;
-
+  
     const fontSize = 20 * this.scaleRatio;
-    this.ctx.font = `${fontSize}px serif`;
-    this.ctx.fillStyle = "#525250";
+    this.ctx.font = `${fontSize}px "Etude Noire"`;
+    this.ctx.fillStyle = "#535353";
+  
     const scoreX = this.canvas.width - 75 * this.scaleRatio;
     const highScoreX = scoreX - 155 * this.scaleRatio;
-
+  
     const scorePadded = Math.floor(this.score).toString().padStart(6, 0);
     const highScorePadded = highScore.toString().padStart(6, 0);
-
+  
     this.ctx.fillText(scorePadded, scoreX, y);
     this.ctx.fillText(`Рекорд ${highScorePadded}`, highScoreX, y);
   }
